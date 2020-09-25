edf3961f95ceb22e83bceb61ab5677cd1157b50b
Hassan Farooqui <50511629+HFhassan@users.noreply.github.com>
2020-09-25 21:45:01 +0500

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,6 +5,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
   Label, Button, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
 
 const required = (val) => val && val.length;
 const maxLength = len => val => !(val) || val.length <= len;
@@ -51,10 +53,30 @@ const RenderComments = ({comments, addComment, dishId}) => {
 
 const DishDetail = (props) => {
   let dish = props.dish
+        
+  if (props.isLoading) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <Loading />
+            </div>
+        </div>
+    );
+}
+else if (props.errMess) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <h4>{props.errMess}</h4>
+            </div>
+        </div>
+    );
+}
 
-  if (dish == null)
+
+else  if (dish == null)
     return <div></div>
-
+else
   return (
     <div className="container">
       <div className="row">
