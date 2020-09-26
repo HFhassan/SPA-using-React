20f24191a62409665ec18c13796e85844f1c75b7
Hassan Farooqui <50511629+HFhassan@users.noreply.github.com>
2020-09-27 01:05:14 +0500

    connected json to the project

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -6,7 +6,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = len => val => !(val) || val.length <= len;
@@ -15,8 +15,8 @@ const minLength = len => val => !(val) || val.length >= len;
 const RenderDish = ({dish}) => {
   return (
       <Card>
-        <CardImg top src={dish.image} alt={dish.name}/>
-        <CardBody>
+
+      <CardImg top src={baseUrl + dish.image} alt={dish.name} />        <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
         </CardBody>
